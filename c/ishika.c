08ae2d78649a8dabcb8b95600680820511bfb992
ishika <ishikasarhadi16@gmail.com>
2025-02-12 10:52:13 +0530

    add

---
+++ c/ishika.c
@@ -0,0 +1,95 @@
+/*#include<stdio.h>
+void main()
+{
+    int x=10;
+    printf("%d",x);
+}*/
+
+/*#include<stdio.h>
+void main()
+{
+    int x=20;
+    int y=10;
+    x=x+10;
+    y=y+10;
+    printf("%d",x);
+    printf("\n%d",y);
+
+
+
+
+
+}*/
+
+/*nclude<stdio.h>
+void main()
+{
+    int x;
+    printf("enter the num:");
+    scanf("%d",x);
+}
+
+#include<stdio.h>
+int main()
+{
+    int x;
+    char y;
+    x=-1;
+    y=-2;
+    printf("%d,%d",x,y);
+    return 0;
+}
+
+#include<stdio.h>
+void main()
+{
+    printf("hello world");
+}
+
+#include<stdio.h>
+int main()
+{
+    printf("hello chitkara");
+    return 0;
+}
+
+#include<stdio.h>
+void main()
+{
+    printf("Hello World!");
+}
+#include<stdio.h>
+int main()
+{
+    char ch;
+    char str[30];
+    char sen[100];
+    scanf("%c",&ch);
+    scanf("%s",&str);
+    scanf("%s",&sen);
+    printf("%c\n",ch);
+    printf("%s\n",str);
+    printf("%s",sen);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the input:");
+    scanf("%d",&a);
+    printf("output: %d",a);
+    return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter the age:");
+    scanf("%d",&age);
+    return 0;
+    
+
+}
---
+++ c/prac.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("enter the charcter:");
+    scanf("%c",&ch);
+    printf("ascii value %c is: %d",ch, ch);
+    return 0;
+}
